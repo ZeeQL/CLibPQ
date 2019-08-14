07870a698fb57ba8f03e5aa32dd2a49f96db65a0
Helge Heß <devteam@zeezide.de>
2019-08-14 16:58:12 +0200

    Use hardcoded libpq path
    ...

--- Package.swift
+++ Package.swift
@@ -4,6 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "CLibPQ",
+    /* only in system module packages
+    pkgConfig: "libpq",
+    providers: [
+      .brew([ "postgresql" ]),
+      .apt([ "libpq-dev"])
+    ],
+    */
     products: [
         .library(name: "CLibPQ", targets: ["CLibPQ"])
     ],
--- Sources/CLibPQ/CLibPQShim.h
+++ Sources/CLibPQ/CLibPQShim.h
@@ -1,12 +1,12 @@
 #ifndef __CLibPQ_SHIM_H__
 #define __CLibPQ_SHIM_H__
 
-#if 1 // let us assume that pkgconfig works
+#if 0 // let us assume that pkgconfig works - doesn't in this setup!
 #  include <libpq-fe.h>
 #else
 #  if defined(__APPLE__) && defined(__MACH__)
      // this assumes brew
-#    include "/usr/local/opt/postgresql/include/libpq-fe.h"
+#    include "/usr/local/opt/libpq/include/libpq-fe.h"
 #  else
 #    include "/usr/include/postgresql/libpq-fe.h"
 #  endif
